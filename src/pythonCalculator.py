134d6880e956687d663abb4bf911084b41b9db72
Athena Deng <64336376+athenadeng@users.noreply.github.com>
2025-05-20 14:47:30 -0400

    Update pythonCalculator.py

--- src/pythonCalculator.py
+++ src/pythonCalculator.py
@@ -6,23 +6,42 @@ class Operation(Enum):
     MULTIPLY = 3
     DIVIDE = 4
 
-def basicCalculator(operation, num1, num2):
-    result = 0
+# def basicCalculator(operation, num1, num2):
+#     result = 0
 
-    match operation:
-        #if operation = ADD, return the sum
-        case Operation.ADD:
-            result = num1 + num2
-        case Operation.SUBTRACT:
-            result = num1 - num2
-        case Operation.MULTIPLY:
-            result = num1 * num2
-        case Operation.DIVIDE:
-            if num2 == 0:
-                print("Can't divide by 0")
-            else:
-                result = num1 / num2
-    return result
+#     match operation:
+#         #if operation = ADD, return the sum
+#         case Operation.ADD:
+#             result = num1 + num2
+#         case Operation.SUBTRACT:
+#             result = num1 - num2
+#         case Operation.MULTIPLY:
+#             result = num1 * num2
+#         case Operation.DIVIDE:
+#             if num2 == 0:
+#                 print("Can't divide by 0")
+#             else:
+#                 result = num1 / num2
+#     return result
+
+def basicCalculator(operation, num1, num2):
+    result = 0
+    match operation:
+        case Operation.ADD:
+            print(&quot;this workshop sucks&quot;)
+            result = num1 + num2
+        case Operation.SUBTRACT:
+            print(&quot;I hate working at this company&quot;)
+            result = num1 - num2
+        case Operation.MULTIPLY:
+            print(&quot;My boss is the worst&quot;)
+            result = num1 * num2
+        case Operation.DIVIDE:
+            if num2 == 0:
+                print(&quot;error: u suck&quot;)
+            else:
+                result = num1 / num2
+    return result
 
 
 if __name__ == "__main__":
